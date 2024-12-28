dc2b960b1b5dc4a619bab800179d6fbba4a53a84
Priya Karn <priya.karn.rn51@gmail.com>
2024-12-28 22:05:29 +0530

    remove unwanted files or folder

--- server/router/user-router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const authMiddleware = require('../middleware/auth-middleware');
-const userController = require('../controllers/user-controller');
-const userRouter = express.Router();
-
-userRouter.route("/user").get(authMiddleware,userController);
-
-module.exports = userRouter;
